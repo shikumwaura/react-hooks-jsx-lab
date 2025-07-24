7244b52cee9158e2a8f8c095a8f3a6111e22b720
Wanjiku <wanjikumwaura0304@gmail.com>
2025-07-24 06:06:49 +0300

    Complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { image } from "../data/data";
 
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a brief description about me.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
+
 
 export default About;
